4059cd0503705efff93e48330026f0772a11caa0
Narbeh Shahnazarian <me@narb.dev>
2024-12-20 15:04:11 -0800

    Adds deployer script

---
+++ script/LilypadToken.s.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+import {LilypadToken} from "../src/LilypadToken.sol";
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+
+contract DeployLilypadToken is Script {
+    function run() external returns (address) {
+        vm.startBroadcast();
+
+        // TODO: Change this to the actual initial supply once we have it
+        uint256 initialSupply = 1_670_000 * 10**18;
+        LilypadToken implementation = new LilypadToken(initialSupply);
+
+        vm.stopBroadcast();
+
+        return address(implementation);
+    }
+}
