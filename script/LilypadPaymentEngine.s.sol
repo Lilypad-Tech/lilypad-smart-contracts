856d7614c541de778ddc55e986b4b3700b40ab46
Narbeh Shahnazarian <me@narb.dev>
2025-02-19 15:04:03 -0800

    Updates paymentEngine deploy script for tokenomics contract

--- script/LilypadPaymentEngine.s.sol
+++ script/LilypadPaymentEngine.s.sol
@@ -6,6 +6,7 @@ import {LilypadPaymentEngine} from "../src/LilypadPaymentEngine.sol";
 import {LilypadToken} from "../src/LilypadToken.sol";
 import {LilypadStorage} from "../src/LilypadStorage.sol";
 import {LilypadUser} from "../src/LilypadUser.sol";
+import {LilypadTokenomics} from "../src/LilypadTokenomics.sol";
 import {SharedStructs} from "../src/SharedStructs.sol";
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
@@ -30,41 +31,47 @@ contract DeployLilypadPaymentEngine is Script {
         return (LilypadUser(address(proxy)), address(proxy));
     }
 
+    function deployTokenomics() internal returns (LilypadTokenomics, address) {
+        LilypadTokenomics tokenomicsImpl = new LilypadTokenomics();
+        bytes memory initData = abi.encodeWithSelector(LilypadTokenomics.initialize.selector);
+        ERC1967Proxy proxy = new ERC1967Proxy(address(tokenomicsImpl), initData);
+        return (LilypadTokenomics(address(proxy)), address(proxy));
+    }
+
     function deployEngine(
         address token,
         address storage_,
         address user_,
+        address tokenomics,
         address treasury,
         address rewards,
         address validationPool
     ) internal returns (LilypadPaymentEngine, address) {
         LilypadPaymentEngine engineImpl = new LilypadPaymentEngine();
         bytes memory initData = abi.encodeWithSelector(
-            LilypadPaymentEngine.initialize.selector, token, storage_, user_, treasury, rewards, validationPool
+            LilypadPaymentEngine.initialize.selector, token, storage_, user_, tokenomics, treasury, rewards, validationPool
         );
         ERC1967Proxy proxy = new ERC1967Proxy(address(engineImpl), initData);
         return (LilypadPaymentEngine(address(proxy)), address(proxy));
     }
 
     function run() external {
-        // These are test wallets from anvil for testing purposes
-        address treasuryWallet = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
-        address valueRewardsWallet = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
-        address validationPoolWallet = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
-
         vm.startBroadcast();
 
         // Deploy contracts
         LilypadToken token = deployToken();
         (LilypadStorage lilypadStorage, address storageProxy) = deployStorage();
         (LilypadUser user, address userProxy) = deployUser();
+        (LilypadTokenomics tokenomics, address tokenomicsProxy) = deployTokenomics();
         (LilypadPaymentEngine paymentEngine, address engineProxy) = deployEngine(
             address(token),
             address(lilypadStorage),
             address(user),
-            treasuryWallet,
-            valueRewardsWallet,
-            validationPoolWallet
+            address(tokenomics),
+            // These are test wallets from anvil for testing purposes
+            0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266, // treasury
+            0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266, // valueRewards
+            0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 // validationPool
         );
 
         // Setup roles
@@ -76,6 +83,7 @@ contract DeployLilypadPaymentEngine is Script {
         console.log("Token:", address(token));
         console.log("Storage:", storageProxy);
         console.log("User:", userProxy);
+        console.log("Tokenomics:", tokenomicsProxy);
         console.log("Payment Engine:", engineProxy);
 
         vm.stopBroadcast();
