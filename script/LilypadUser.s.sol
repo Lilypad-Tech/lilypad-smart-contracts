c3146121cd795f0efecfdefdeb54abab7d36e6fe
Narbeh Shahnazarian <me@narb.dev>
2025-02-21 14:52:43 -0800

    Updates LilypadUser to deploy with OZ Upgrade Library
    - Changes the deploy script for the LilypadUser contraact to utilize the OpenZepplin Upgrade library to properly deploy a TransparentProxy
    - Once this deployment is done, the resulting proxy address will need to be recorded somehwere as it will be the primary address other contracts/users will be expected to interact with.  When a future upgrade happens we will need to provide this proxy address as input so that the upgrade can point the proxy to the new implmentation
    - Updates foundry.toml file according to : https://docs.openzeppelin.com/upgrades-plugins/foundry-upgrades

--- script/LilypadUser.s.sol
+++ script/LilypadUser.s.sol
@@ -3,10 +3,14 @@ pragma solidity ^0.8.24;
 
 import "forge-std/Script.sol";
 import {LilypadUser} from "../src/LilypadUser.sol";
-import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
 
 contract DeployLilypadUser is Script {
     function run() external returns (address) {
+        //Note: This is the address[0] from anvil only meant for testing
+        //TODO: Change this to the address of the initial owner
+        address initialOwner = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
+
         vm.startBroadcast();
 
         // 1. Deploy the implementation contract
@@ -15,10 +19,16 @@ contract DeployLilypadUser is Script {
         // 2. Encode the initialization data
         bytes memory initData = abi.encodeWithSelector(LilypadUser.initialize.selector);
 
-        // 3. Deploy the proxy contract pointing to the implementation
-        ERC1967Proxy proxy = new ERC1967Proxy(address(implementation), initData);
+        // 3. Deploy the proxy contract pointing to the implementation.  Take note of the proxy address that is returned as this will be the address users will interact with
+        address proxy = Upgrades.deployTransparentProxy(
+            "LilypadUser.sol",
+            initialOwner,
+            initData
+        );
 
         vm.stopBroadcast();
+
+        console.log("LilypadUser deployed at:", address(proxy));
 
         // Return the proxy address - this is the address users will interact with
         return address(proxy);
