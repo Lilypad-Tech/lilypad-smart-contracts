48f112c7d0f86a17cac3ba59d98d7953e541e0b2
Narbeh Shahnazarian <me@narb.dev>
2025-02-24 09:06:20 -0800

    Updates module directory deploy script to use Upgrades library

--- script/LilypadModuleDirectory.s.sol
+++ script/LilypadModuleDirectory.s.sol
@@ -5,20 +5,28 @@ import "forge-std/Script.sol";
 import {LilypadModuleDirectory} from "../src/LilypadModuleDirectory.sol";
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import {LilypadUser} from "../src/LilypadUser.sol";
+import {Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
 
 contract DeployLilypadModuleDirectory is Script {
     function run() external returns (address) {
+        //Note: This is the address[0] from anvil only meant for testing
+        //TODO: Change this to the address of the initial owner
+        address initialOwner = vm.envAddress("INITIAL_OWNER_ADDRESS_FOR_PROXY_ADMIN");
+
+        //TODO: Add the actual address of the deployed lilypad user proxy contract here
+        address lilypadUserProxyAddress = vm.envAddress("LILYPAD_USER_PROXY_ADDRESS");
+
         vm.startBroadcast();
 
-        // 1. Deploy the implementation contract
-        LilypadModuleDirectory implementation = new LilypadModuleDirectory();
-        LilypadUser lilypadUser = new LilypadUser();
+        // 1. Encode the initialization data
+        bytes memory initData = abi.encodeWithSelector(LilypadModuleDirectory.initialize.selector, lilypadUserProxyAddress);
 
-        // 2. Encode the initialization data
-        bytes memory initData = abi.encodeWithSelector(LilypadModuleDirectory.initialize.selector, address(lilypadUser));
-
-        // 3. Deploy the proxy contract pointing to the implementation
-        ERC1967Proxy proxy = new ERC1967Proxy(address(implementation), initData);
+        // 2. Deploy the proxy contract pointing to the implementation
+        address proxy = Upgrades.deployTransparentProxy(
+            "LilypadModuleDirectory.sol",
+            initialOwner,
+            initData
+        );
 
         vm.stopBroadcast();
 
